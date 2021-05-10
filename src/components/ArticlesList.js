a2e324ed193894f384cbd14d5c7fd97dc1940ed3
Tosinnijis1 <adenijitosin112@gmail.com>
2021-05-10 10:19:38 -0400

    done

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,14 +1,12 @@
 import { Link } from 'react-router-dom'
 
-
 const ArticlesList = ({ articles }) => (
     <>
     {articles.map((article, key) => (
         <Link id="articles-list-item" key={key} to={`/articles/${article.name}`}>
          <h3> {article.title} </h3>
          <p> {article.content[0].substring(0, 70)}... </p>
-        </Link>
-        
+        </Link>       
     ))}
     </>
 );
